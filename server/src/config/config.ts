79da15556ad3cc00a88401a3b703e98462f4250e
AntonPavlovich <antopavlu@gmail.com>
2023-10-18 22:19:05 +0300

    update fconfig file add variables for weather api

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -6,10 +6,20 @@ const {
   POSTGRES_PASSWORD,
   DATABASE_PORT,
   DATABASE_HOST,
+  APP_PORT,
+  WEATHER_API_KEY,
+  WEATHER_API_URL
 } = process.env;
 
 interface Config {
   database: PostgresConnectionOptions;
+  common: {
+    appPort: number
+  },
+  weatherApi: {
+    apiKey: string,
+    baseUrl: string
+  }
 }
 
 export const config = {
@@ -20,7 +30,14 @@ export const config = {
     username: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
     database: POSTGRES_DB,
-    entities: [''],
-    migrations: [''],
+    entities: ['./dist/database/entities/*.js'],
+    migrations: ['./dist/database/migrations/*.js'],
   },
+  common: {
+    appPort: APP_PORT ?? 5000
+  },
+  weatherApi: {
+    apiKey: WEATHER_API_KEY,
+    baseUrl: WEATHER_API_URL
+  }
 } as Config;
