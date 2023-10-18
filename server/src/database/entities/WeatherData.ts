8b9814995ca957f2262fc606f73679a8280a1a46
AntonPavlovich <antopavlu@gmail.com>
2023-10-19 00:18:55 +0300

    add new new colums to the Weather table

--- server/src/database/entities/WeatherData.ts
+++ server/src/database/entities/WeatherData.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('weather_data')
 export class WeatherData {
@@ -6,8 +6,24 @@ export class WeatherData {
   id: number;
 
   @Column({
+    type: 'decimal',
+  })
+  lat: number
+
+  @Column({
+    type: 'decimal'
+  })
+  lon: number
+
+  @Column({
     type: 'jsonb',
     nullable: false
   })
   data: string
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => "CURRENT_TIMESTAMP(6)"
+  })
+  createdAt: Date
 }
---
+++ server/src/database/migrations/1697663864722-AddCreatedAtColumnToTheWeatherTable.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddCreatedAtColumnToTheWeatherTable1697663864722 implements MigrationInterface {
+    name = 'AddCreatedAtColumnToTheWeatherTable1697663864722'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "weather_data" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "weather_data" DROP COLUMN "createdAt"`);
+    }
+
+}
